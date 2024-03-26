9a6b51141bdc2872668e975530ee83fdb1e6b0a2
Alex <alex.wang@gofreight.com>
2024-03-26 17:52:13 +0800

    first try for Observable

---
+++ app.js
@@ -0,0 +1,26 @@
+// rxjs is global variable
+
+const observable = new rxjs.Observable((subscriber) => {
+  console.log('Hi')
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
+});
+
+console.log('just before subscribe');
+observable.subscribe({
+  next(x) {
+    console.log('got value ' + x);
+  },
+  error(err) {
+    console.error('something wrong occurred: ' + err);
+  },
+  complete() {
+    console.log('done');
+  },
+});
+console.log('just after subscribe');
